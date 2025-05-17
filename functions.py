eca3653436af5433537080e26ae60b6b3a338cf7
Aayush Amit Patil <patil.aayush.amit.sheetal@gmail.com>
2025-05-16 23:52:55 -0400

    V.56
    NEw iter swithc LW in tryProduct

--- functions.py
+++ functions.py
@@ -1710,8 +1710,8 @@ def try_place_product(product, container, container_placed, occupied_volume, pla
     """
     for orientation in get_orientations(product):
         l, w, h = orientation
-        for y in range(0, math.floor(container['Width'] - w)):
-            for x in range(0, math.floor(container['Length'] - l)):
+        for y in range(0, math.floor(container['Width'] - l)):
+            for x in range(0, math.floor(container['Length'] - w)):
                 for z in range(0, math.floor(container['Height'] - h)):
                     if fits(container, container_placed, x, y, z, l, w, h):
                         product_data = {
